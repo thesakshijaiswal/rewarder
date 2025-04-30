28926387ff3acd3209f2d0f6f34f2dbd14a9a184
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-04-30 17:18:17 +0530

    fix: add credit routes for handling credit-related API requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
+import creditRoutes from "./routes/credit.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/credits", creditRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is Running 🟢");
