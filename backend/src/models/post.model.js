018bcc86b4977f2b19432d280947b8032ce0e499
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-04-30 17:18:45 +0530

    feat: add post model schema for managing posts in the application

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    enum: ["twitter", "reddit"],
+    required: true,
+  },
+  originalId: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  imageUrl: String,
+  savedBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  reportedBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  reportReasons: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      reason: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
