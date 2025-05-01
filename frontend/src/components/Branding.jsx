bf0e6c9891fa3259b5b4514599350907e2b2c456
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-05-01 19:46:43 +0530

    feat: update Branding component to include rewarder logo and improve layout

--- frontend/src/components/Branding.jsx
+++ frontend/src/components/Branding.jsx
@@ -1,8 +1,12 @@
+import rewarderLogo from "../assets/rewarder.svg";
 const Branding = () => {
   return (
-    <h1 className="text-center text-3xl font-extrabold text-indigo-500">
-      Rewarder
-    </h1>
+    <div className="flex items-center justify-center gap-2">
+      <img src={rewarderLogo} alt="logo" className="h-10 w-10" />
+      <h1 className="text-center text-3xl font-extrabold text-indigo-500">
+        Rewarder
+      </h1>
+    </div>
   );
 };
 
