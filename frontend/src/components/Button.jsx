b29392befb296dfc2c2d558792897df934b9b0d5
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-05-03 15:55:10 +0530

    feat: enhance Button component to support dynamic button type and disabled state styling

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,13 +3,18 @@ const Button = ({
   btnIcon: Icon = null,
   children = "",
   className = "",
+  disabled = false,
+  type = "button",
 }) => {
   return (
     <>
       {(children || Icon) && (
         <button
+          type={type}
           onClick={onClick}
-          className={`flex items-center justify-center gap-2 rounded-md px-4 py-[6px] text-base shadow-md ${className}`}
+          className={`flex items-center justify-center gap-2 rounded-md px-4 py-[6px] text-base shadow-md ${className} ${
+            disabled ? "cursor-not-allowed opacity-50" : ""
+          }`}
         >
           {Icon && <Icon className="text-xl" />}
           {children}
